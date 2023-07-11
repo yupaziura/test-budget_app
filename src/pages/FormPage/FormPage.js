1ec18443a9f46465944ce79196b014544f42eca3
YuliiaPaziura <yuliiapaziura@gmail.com>
2023-07-11 14:05:23 +0300

    Add styles to form page

--- src/pages/FormPage/FormPage.js
+++ src/pages/FormPage/FormPage.js
@@ -1,9 +1,12 @@
-import CustomForm from "../../components/Form/CustomForm"
+import CustomForm from "../../components/Form/CustomForm";
+
+import './FormPage.scss';
 
 const FormPage = ({setData, data}) => {
     return (
         <>
-            <h4>Form</h4>
+            <h3 className="form-page_header">Form</h3>
+            <p className="form-page_text">Please, carefully fill this form to provide new transaction.</p>
             <CustomForm setData={setData} data={data}/>
         </>
     )
