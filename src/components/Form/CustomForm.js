41c33ca20a7dd8a4dfd9290a8bfafccda8f3fa95
YuliiaPaziura <yuliiapaziura@gmail.com>
2023-07-11 10:15:11 +0300

    Create form inputs

--- src/components/Form/CustomForm.js
+++ src/components/Form/CustomForm.js
@@ -1,7 +1,43 @@
+import { TextField, Select, Button, InputLabel, MenuItem, FormControl } from "@mui/material";
+
 const CustomForm = () => {
     return (
         <>
-            Form
+            <form action="">
+
+                <div className="amount">
+                    <TextField required id="amount" label="Amount" variant="standard" />
+                </div>
+
+                <div className="type">
+                    <FormControl variant="standard" >
+                        <InputLabel required id="type-label">Transaction type</InputLabel>
+                        <Select
+                        id="transaction-type"
+                        label="Transaction type"
+                        >
+                            <MenuItem value={'expenses'}>
+                                <em>Expenses</em>
+                            </MenuItem>
+                            <MenuItem value={'income'}>Income</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
+
+                <div className="comment">
+                    <TextField
+                        id="comment"
+                        label="Comment"
+                        multiline
+                        rows={4}
+                        // defaultValue="Default Value"
+                        variant="standard"
+                    />
+                </div>
+
+                <Button type='submit' variant="outlined">Add transaction</Button>
+
+            </form>
         </>
     )
 }
