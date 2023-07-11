1ae36bfc4de10501bb4e3ee7e54dca8ae9eb73c6
YuliiaPaziura <yuliiapaziura@gmail.com>
2023-07-11 14:01:40 +0300

    Add styles to form

--- src/components/Form/CustomForm.js
+++ src/components/Form/CustomForm.js
@@ -11,6 +11,8 @@ import { TextField,
          FormControl } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 
+import './CustomForm.scss';
+
 const CustomForm = ({setData, data}) => {
     const [amount, setAmount] = useState('');
     const [type, setType] = useState('expenses');
@@ -40,48 +42,58 @@ const CustomForm = ({setData, data}) => {
 
     return (
         <>
-            <form onSubmit={(e)=>pushToData(e)}>
+            <form className="form" onSubmit={(e)=>pushToData(e)}>
 
-                <div className="amount">
-                    <TextField  type='number' inputProps={{ min: 1 }} value={amount} onChange={(e)=>setAmount(+e.target.value)} required id="amount" label="Amount" variant="standard"/>
+                <div className="form_col">
+                    <div className="amount">
+                        <TextField fullWidth  type='number' inputProps={{ min: 1 }} value={amount} onChange={(e)=>setAmount(+e.target.value)} required id="amount" label="Amount" variant="outlined"/>
+                    </div>
+
+                    <div className="type">
+                        <FormControl fullWidth variant="outlined">
+                            <InputLabel required id="type-label">Transaction type</InputLabel>
+                            <Select
+                                value={type}
+                                id="transaction-type"
+                                label="Transaction type"
+                                onChange={(e)=>selectType(e)}
+                            >
+                                <MenuItem value={'expenses'}> Expenses</MenuItem>
+                                <MenuItem value={'income'}>Income</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
+
+                    <div className="date">
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker style={{width: '100%'}} onChange={(newValue) => setDate(newValue.toISOString())}
+                                        value={date} 
+                                        format='DD/MM/YYYY'
+                                        slotProps={{ textField: { fullWidth: true } }}/>
+                        </LocalizationProvider>
+                    </div>
                 </div>
 
-                <div className="type">
-                    <FormControl variant="standard">
-                        <InputLabel required id="type-label">Transaction type</InputLabel>
-                        <Select
-                            value={type}
-                            id="transaction-type"
-                            label="Transaction type"
-                            onChange={(e)=>selectType(e)}
-                        >
-                            <MenuItem value={'expenses'}> Expenses</MenuItem>
-                            <MenuItem value={'income'}>Income</MenuItem>
-                        </Select>
-                    </FormControl>
+
+                <div className="form_col">
+                    <div className="comment">
+                        <TextField
+                            onChange={(e)=>setComment(e.target.value)}
+                            value={comment}
+                            id="comment"
+                            label="Comment"
+                            multiline
+                            rows={4}
+                            variant="outlined"
+                            fullWidth
+                        />
+                    </div>
                 </div>
 
-                <div className="date">
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker onChange={(newValue) => setDate(newValue.toISOString())}
-                                value={date} 
-                                format='DD/MM/YYYY'/>
-                </LocalizationProvider>
+                <div class="break"></div>
+                <div className="button">
+                    <Button fullWidth type='submit' variant="outlined">Add transaction</Button>
                 </div>
-
-                <div className="comment">
-                    <TextField
-                        onChange={(e)=>setComment(e.target.value)}
-                        value={comment}
-                        id="comment"
-                        label="Comment"
-                        multiline
-                        rows={4}
-                        variant="standard"
-                    />
-                </div>
-
-                <Button type='submit' variant="outlined">Add transaction</Button>
 
             </form>
         </>
